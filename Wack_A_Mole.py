7904b4f9c67c7b5ba2c6e47523d0b15d5a5bcfc6
laptopcoder11-school <106130005+laptopcoder11-school@users.noreply.github.com>
2024-08-05 11:57:08 +1000

    Finished Basic Grid Layout UI
    Added a clickable grid to the UI with a linked function when clicked and set up a basic score display.

--- Wack_A_Mole.py
+++ Wack_A_Mole.py
@@ -15,22 +15,53 @@ WINDOW_HIGHT = 800
 WINDOW_X = 600
 WINDOW_Y = 100
 
-#create window class 
-class Window(QMainWindow):
+GAMEBOARD_SIZE = 5
+GRIDBUTTON_SIZE = 70
 
-    #This method initialises the window class
+#Create the WackAMole Game class, inherit from QWidget to allow window display
+class WackAMole(QWidget):
+
+    #This function initialises the window class
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Wack A Mole")
         self.setGeometry(WINDOW_X,WINDOW_Y,WINDOW_WIDTH,WINDOW_HIGHT)
+        self.init_ui()
+
+    #This function initalises the UI
+    def init_ui(self):
+        self.display_button_grid()
+        self.score_label = QLabel('Score: 0', self)
+        self.score_label.setFont(QFont('Arial', 12))
         self.show()
+
+    #This function displays the grid of buttons
+    def display_button_grid(self):
+        #create the layout object and grid list of buttons
+        self.layout = QGridLayout()
+        self.button_grid = [QPushButton(f"{n}") for n in range(GAMEBOARD_SIZE*GAMEBOARD_SIZE)]
+        #set the button size
+        [b.setFixedSize(GRIDBUTTON_SIZE,GRIDBUTTON_SIZE) for b in self.button_grid]
+        #add the buttons to the grid layout
+        [self.layout.addWidget(self.button_grid[(x*GAMEBOARD_SIZE)+y],x,y) for x in range(GAMEBOARD_SIZE) for y in range(GAMEBOARD_SIZE)]
+        #attach a function to the buttons
+        [b.clicked.connect(self.grid_button_clicked) for b in self.button_grid]
+
+        #set the layout
+        self.setLayout(self.layout)
+
+    #This function runs when a button on the grid is clicked
+    def grid_button_clicked(self):
+        button = self.sender()
+        #print the index of the button to the console
+        print(self.button_grid.index(button))
+
 
 #Main Program
 if __name__ == "__main__":
     #Create the QApplication
     app = QApplication(sys.argv)
-    #Create the window object
-    window = Window()
+    #Create the game object
+    game = WackAMole()
     #run the application
     sys.exit(app.exec_())
-
